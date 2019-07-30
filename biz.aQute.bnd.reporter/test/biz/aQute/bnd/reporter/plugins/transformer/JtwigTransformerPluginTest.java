bb1a27fc78cd98fccf2b5a429235d4ea25eb6320
Stefan Bischof <stbischof@bipolis.org>
2019-07-30 17:11:55 +0200

    showSection function
    Signed-off-by: Stefan Bischof <stbischof@bipolis.org>

--- biz.aQute.bnd.reporter/test/biz/aQute/bnd/reporter/plugins/transformer/JtwigTransformerPluginTest.java
+++ biz.aQute.bnd.reporter/test/biz/aQute/bnd/reporter/plugins/transformer/JtwigTransformerPluginTest.java
@@ -2,10 +2,14 @@ package biz.aQute.bnd.reporter.plugins.transformer;
 
 import static org.junit.Assert.assertArrayEquals;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 import aQute.lib.io.IO;
 import biz.aQute.bnd.reporter.plugins.serializer.JsonReportSerializerPlugin;
@@ -61,6 +65,44 @@ public class JtwigTransformerPluginTest extends TestCase {
 				new HashMap<String, String>());
 
 			assertTrue(new String(output.toByteArray()).contains("# test"));
+		}
+	}
+
+	public void testJtwigTransformerShowSection() throws Exception {
+
+		final JtwigTransformerPlugin t = new JtwigTransformerPlugin();
+		final Map<String, Object> report = new HashMap<>();
+		final Map<String, String> parameters = new TreeMap<>();
+
+		parameters.put(JTwigFunctions.BND_REPORTER_SHOW_PREFIX + "optional.paramsetTrue", "true");
+		parameters.put(JTwigFunctions.BND_REPORTER_SHOW_PREFIX + "optional.paramsetFalse", "false");
+		parameters.put(JTwigFunctions.BND_REPORTER_SHOW_PREFIX + "default.paramsetTrue", "TRUE");
+		parameters.put(JTwigFunctions.BND_REPORTER_SHOW_PREFIX + "default.paramsetFalse", "false");
+
+		try (final ByteArrayOutputStream model = new ByteArrayOutputStream()) {
+			new JsonReportSerializerPlugin().serialize(report, model);
+
+			final ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+			t.transform(IO.stream(model.toByteArray()),
+				IO.stream(new File("testresources/transformer/jtwigShowSection.twig")), output, parameters);
+
+			BufferedReader bfReader = new BufferedReader(
+				new InputStreamReader(new ByteArrayInputStream(output.toByteArray())));
+			String temp;
+			int i = 0;
+			while ((temp = bfReader.readLine()) != null) {
+				i++;
+				assertEquals(i + "ok", temp);
+
+			}
+
+			assertArrayEquals(t.getHandledModelExtensions(), new String[] {
+				"json"
+			});
+			assertArrayEquals(t.getHandledTemplateExtensions(), new String[] {
+				"twig", "jtwig"
+			});
 		}
 	}
 }
