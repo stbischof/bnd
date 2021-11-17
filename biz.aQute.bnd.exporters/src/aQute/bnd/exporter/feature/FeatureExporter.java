9705af1c70024ce785ca46da24d64a78f64a1434
Stefan Bischof <stbischof@bipolis.org>
2021-11-17 12:58:36 +0100

    OSGi-Feature-Exporter
    Signed-off-by: Stefan Bischof <stbischof@bipolis.org>

---
+++ biz.aQute.bnd.exporters/src/aQute/bnd/exporter/feature/FeatureExporter.java
@@ -0,0 +1,226 @@
+package aQute.bnd.exporter.feature;
+
+import java.io.File;
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Optional;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import org.osgi.framework.Constants;
+import org.osgi.resource.Capability;
+import org.osgi.service.feature.FeatureBundle;
+import org.osgi.service.feature.FeatureConfiguration;
+import org.osgi.service.feature.FeatureExtension;
+
+import aQute.bnd.annotation.plugin.BndPlugin;
+import aQute.bnd.build.Project;
+import aQute.bnd.exporter.feature.internal.Feature_;
+import aQute.bnd.exporter.feature.internal.ID_;
+import aQute.bnd.exporter.feature.json.FeatureExporterConfig;
+import aQute.bnd.header.Parameters;
+import aQute.bnd.osgi.FileResource;
+import aQute.bnd.osgi.Resource;
+import aQute.bnd.osgi.resource.CapReqBuilder;
+import aQute.bnd.service.export.Exporter;
+import aQute.lib.io.IO;
+
+@BndPlugin(name = "Feature Exporter")
+public class FeatureExporter implements Exporter {
+
+	private Project					project;
+
+	private String					type;
+	private FeatureExporterConfig	exporterConfig	= new FeatureExporterConfig();
+
+	@Override
+	public String[] getTypes() {
+
+		return new String[] {
+			"feature.json"
+		};
+	}
+
+	@Override
+	public Map.Entry<String, Resource> export(String type, final Project project, Map<String, String> options)
+		throws Exception {
+
+		this.project = project;
+		this.type = type;
+		exporterConfig.bundleHashes = parse(options, "bundleHashes");
+		exporterConfig.configurationTemplates = parse(options, "configurationTemplates");
+		exporterConfig.nullFields = parse(options, "nullFields");
+		exporterConfig.structureDocumentation = parse(options, "structureDocumentation");
+		Entry<String, Resource> featureRes = doFeatureFile(exporterConfig);
+		return featureRes;
+	}
+
+	private boolean parse(Map<String, String> options, String key) {
+		return Boolean.parseBoolean(options.getOrDefault(key, "false"));
+	}
+
+	private Map.Entry<String, Resource> doFeatureFile(FeatureExporterConfig cfg) throws Exception {
+
+		Feature_ feature = new Feature_();
+		ID_ fId = new ID_();
+		fId.groupId = "groupId";
+		fId.artifactId = "artefactId";
+		fId.version = version().orElse("0.0.0-INITIAL");
+		feature.id = fId;
+
+		feature.name = name();
+		feature.description = decription();
+		feature.license = license();
+		feature.vendor = vendor();
+
+		feature.configurations = featureConfigurations();
+		feature.bundles = featureBundles();
+		feature.variables = variables();
+		feature.extensions = extensions();
+
+		// FileWriter
+		String fileName = featureName(project);
+		File jsonPath = project.getTargetDir()
+			.toPath()
+			.resolve(fileName)
+			.toFile();
+
+		jsonPath.createNewFile();
+
+		String json = Utils.toJson(feature, exporterConfig);
+		IO.write(json.getBytes(), jsonPath);
+		Resource jsonResource = new FileResource(jsonPath);
+		return new AbstractMap.SimpleEntry<String, Resource>(jsonPath.toString(), jsonResource);
+	}
+
+	private Optional<String> version() {
+
+		return Optional.ofNullable(project.getBundleVersion());
+	}
+
+	private Optional<String> decription() {
+
+		return Optional.ofNullable(project.getBundleDescription());
+	}
+
+	private Optional<String> license() {
+
+		return Optional.ofNullable(project.get(Constants.BUNDLE_LICENSE));
+	}
+
+	private Optional<String> name() {
+
+		return Optional.ofNullable(project.getBundleName());
+	}
+
+	private Optional<String> vendor() {
+
+		return Optional.ofNullable(project.getBundleVendor());
+	}
+
+	private static String featureName(Project project) {
+
+		String pName = project.getName();
+		if ("bnd.bnd".equals(pName)) {
+			return "feature.json";
+		}
+		return "feature-" + pName.replace(".bndrun", ".json");
+	}
+
+	Map<String, FeatureConfiguration> featureConfigurations() {
+
+		return new HashMap<>();
+
+	}
+
+	List<FeatureBundle> featureBundles() {
+
+		try {
+			return Optional.ofNullable(project.getRunbundles()
+				.stream()
+				.map(c -> Utils.toFeatureBundle(c, exporterConfig))
+				.collect(Collectors.toList()))
+				.orElse(new ArrayList<>());
+		} catch (Exception e) {
+
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+
+	}
+
+	Optional<List<Capability>> capabilities() {
+
+		Parameters parameters = project.getProvideCapability();
+		List<Capability> list = CapReqBuilder.getCapabilitiesFrom(parameters);
+		return Optional.ofNullable(list);
+
+	}
+
+	// Optional<List<MatchingRequirement>> requirement() {
+	//
+	// Parameters parameters = project.getRequireCapability();
+	// List<MatchingRequirement> list =
+	// CapReqBuilder.getRequirementsFrom(parameters)
+	// .stream()
+	// .map(MRI::new)
+	// .collect(Collectors.toList());
+	//
+	// return Optional.ofNullable(list);
+	// }
+
+	Map<String, Object> variables() {
+
+		Map<String, Object> vars = project.getProperties()
+			.entrySet()
+			.stream()
+			.filter(isVariable())
+			.collect(Collectors.toMap(e -> e.getKey()
+				.toString(),
+				e -> e.getValue()
+					.toString()));
+		return vars;
+	}
+
+	private Predicate<? super Entry<Object, Object>> isVariable() {
+
+		return e -> {
+			if (e.getKey() == null) {
+				return false;
+			}
+			String key = (String) e.getKey();
+			if (key.toString()
+				.startsWith("-")) {
+				return false;
+			}
+			if (key.startsWith("Bundle-")) {
+				return false;
+			}
+			if (key.startsWith("basedir")) {
+				return false;
+			}
+			if (key.startsWith("Provide-Capability")) {
+				return false;
+			}
+			if (key.startsWith("Require-Capability")) {
+				return false;
+			}
+
+			return true;
+		};
+	}
+
+	private Map<String, FeatureExtension> extensions() {
+
+		Map<String, FeatureExtension> exts = project.getPlugins(ExtensionPlugin.class)
+			.stream()
+			.collect(Collectors.toMap(p -> p.toExtension()
+				.getName(), ExtensionPlugin::toExtension));
+
+		return exts;
+	}
+}
--- biz.aQute.bndlib/src/aQute/bnd/osgi/Builder.java
+++ biz.aQute.bndlib/src/aQute/bnd/osgi/Builder.java
@@ -1290,9 +1290,10 @@ public class Builder extends Analyzer {
 		// optionally appended with a modified regular expression
 		// like @zip.jar!/META-INF/MANIFEST.MF
 		int n = source.lastIndexOf("!/");
+		String cleanSource = source.substring(n + 2);
 		Instruction instr = null;
 		if (n > 0) {
-			instr = new Instruction(source.substring(n + 2));
+			instr = new Instruction(cleanSource);
 			source = source.substring(0, n);
 		}
 
@@ -1317,6 +1318,7 @@ public class Builder extends Analyzer {
 
 			for (Jar j : sub)
 				addAll(jar, j, instr, destination, nameMapper);
+
 		}
 	}
 
@@ -1342,6 +1344,7 @@ public class Builder extends Analyzer {
 	}
 
 	private boolean addAll(Jar to, Jar sub, Instruction filter, String destination, Function<String, String> modifier) {
+
 		boolean dupl = false;
 		for (String name : sub.getResources()
 			.keySet()) {
